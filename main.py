0f73dafb7292798bf43fa107707f624b6c481ff1
HARSIS <91324320+HARSIS@users.noreply.github.com>
2022-09-27 14:03:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 class Player:
     def __init__(self, Phone_Number, Name_SurN, Age, Type, Camouflage):
-        self.Phone_Number = Phone_Number
-        self.Name_SurN = Name_SurN
-        self.Age = Age
-        self.Type = Type
-        self.Camouflage = Camouflage
+        self.Phone_Number = Phone_Number #телефон
+        self.Name = Name #имя
+        self.Surn = Surn #фамилия
+        self.Age = Age #возрост
+        self.Type = Type #тип
+        self.Camouflage = Camouflage #позывной
 
     def Get_Phone_Number(self):
         return self.Phone_Number
 
-    def Get_Name_SurN(self):
-        return self.Name_SurN
+    def Get_Name(self):
+        return self.Name
+    
+    def Get_Surn(self):
+        return self.Surn
 
     def Get_Age(self):
         return self.Age
@@ -24,8 +28,11 @@ class Player:
     def Set_Phone_Number(self, a):
         self.Phone_Number = a
 
-    def Set_Name_SurN(self, a):
-        self.Name_SurN = a
+    def Set_Name(self, a):
+        self.Name = a
+        
+    def Set_Surn(self, a):
+        self.Surn = a
 
     def Set_Age(self, a):
         self.Age = a
